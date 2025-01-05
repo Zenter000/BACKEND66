072616dbcfb8edff49aec53150387772496bbb76
Zenter000 <mosyertop@gmail.com>
2025-01-05 16:21:57 +0700

    set1

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -1,5 +1,9 @@
-const express =  require('exepress');
+const express =  require('express');
 const app = express()
+
+app.get('/anyweather',(req,res) =>{
+    res.sendFile(__dirname+"/weather.html")
+})
 app.listen(3000,()=>{
     console.log("Server started on port 3000!")
 })
